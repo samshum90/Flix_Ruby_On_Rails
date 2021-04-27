a3da16dd5028fb5beb9175fba223a070f96a29ac
Sam <samshum90@gmail.com>
2021-04-27 15:48:11 +0100

    completed section 14

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,4 +9,25 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie)
+  end
+  def new
+      @movie = Event.new
+  end
+  def create
+      @movie = Event.new(movie_params) 
+      @movie.save
+      redirect_to @movie
+  end
+    
+private
+
+    def movie_params
+        params.require(:movie).
+            permit(:title, :description, :rating, :released_on, :total_gross)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 roots "movies#index"
-get "movies" => "movies#index"
-get "movies/:id" => "movies#show", as: "movie"
-get "movies/:id/edit" => "movie#edit", as: "edit_movie"
+
+resources :movies
 end
